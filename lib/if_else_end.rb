9808ba941989612908f77fcde45ce103a9fd4a59
Michael Iacono <iaconom821@gmail.com>
2021-02-15 17:11:23 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,17 +1,8 @@
 # Write your solution here
 
-run_code_inside = false
-puts "Code before if...end"
-if run_code_inside 
-  puts "code inside"
-end
-puts "Code after if...end"
-
-chance_of_rain = 0.2 
-puts "Let's go outside!"
-if chance_of_rain > 0.5
-  puts "Pack an umbrella"
+current_time = Time.now 
+if current_time.to_i % 2 == 0 
+  puts "Even!"
 else
-  puts "Enjoy the fine day!"
+  puts "Odd!"
 end
-puts "Oh, and always wear sunscreen"
